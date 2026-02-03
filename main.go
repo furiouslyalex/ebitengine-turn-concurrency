a1153437dd266d798f7e92f8afb6c0caeb099eab
Alex <phazedplasma@gmail.com>
2026-02-03 13:45:17 -0700

    adding + drawing images

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image"
 	"image/color"
 	"log"
 
@@ -8,16 +9,36 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Sprite struct {
+	Image *ebiten.Image
+	X, Y  float64
+}
+
+type Game struct {
+	Player  *Sprite
+	Enemies []*Sprite
+}
 
 func (g *Game) Update() error {
 	return nil
+
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-
 	screen.Fill(color.RGBA{120, 180, 255, 255})
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+
+	// draw player
+	opts := ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(g.Player.X, g.Player.Y)
+	screen.DrawImage(g.Player.Image.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image), &opts)
+	opts.GeoM.Reset()
+
+	// draw enemies
+	for _, e := range g.Enemies {
+		opts.GeoM.Translate(e.X, e.Y)
+		screen.DrawImage(e.Image.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image), &opts)
+		opts.GeoM.Reset()
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -26,8 +47,45 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+	// load sprites
+	playerImage, _, err := ebitenutil.NewImageFromFile("assets/images/ninja.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	gladiatorImage, _, err := ebitenutil.NewImageFromFile("assets/images/gladiator.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	skeletonImage, _, err := ebitenutil.NewImageFromFile("assets/images/skeleton.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lionImage, _, err := ebitenutil.NewImageFromFile("assets/images/lion.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := ebiten.RunGame(&Game{Player: &Sprite{
+		Image: playerImage,
+		X:     100,
+		Y:     100,
+	}, Enemies: []*Sprite{
+		{
+			Image: gladiatorImage,
+			X:     75,
+			Y:     75,
+		}, {
+			Image: skeletonImage,
+			X:     150,
+			Y:     150,
+		}, {
+			Image: lionImage,
+			X:     50,
+			Y:     50,
+		},
+	}}); err != nil {
 		log.Fatal(err)
 	}
 }
